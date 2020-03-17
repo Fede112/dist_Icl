1487149eb28cf756de69d9129438bb2330dfd33b
Fede <fedebarone@gmail.com>
2020-03-17 16:22:07 +0100

    Testing threads

---
+++ threads.c
@@ -0,0 +1,39 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <pthread.h>
+
+#define N 4
+
+void *worker_thread(void *arg)
+{
+        printf("This is worker_thread #%ld\n", (long)arg);
+        pthread_exit(NULL);
+}
+
+void *consumer(void *arg)
+{
+        printf("This is worker_thread #%ld\n", (long)arg);
+        pthread_exit(NULL);
+}
+
+void *producer(void *arg)
+{
+        printf("This is worker_thread #%ld\n", (long)arg);
+        pthread_exit(NULL);
+}
+
+int main()
+{
+    pthread_t my_thread[N];
+
+    long id;
+    for(id = 1; id <= N; id++) {
+        int ret =  pthread_create(&my_thread[id], NULL, &worker_thread, (void*)id);
+        if(ret != 0) {
+            printf("Error: pthread_create() failed\n");
+            exit(EXIT_FAILURE);
+        }
+    }
+
+    pthread_exit(NULL);
+}
