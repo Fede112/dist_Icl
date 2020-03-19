b3fb1d2d1e5530a415a6c5f667f48ccb6ad9c3b9
Elena Tea <elenatea.russo@gmail.com>
2020-03-19 15:07:30 +0100

    Update BASICinput2binary.cc

--- aux/BASICinput2binary.cc
+++ aux/BASICinput2binary.cc
@@ -3,7 +3,7 @@
 #include <fstream>
 #include <sstream>
 
-#include "cxxopts.hpp"
+
 using namespace std;
 
 
@@ -11,46 +11,11 @@ int main(int argc, char** argv)
 {
 
 
-    ////////////////////////////////////////////////////////////////////////
-    // Args parser (I tried a new library but I am not convinced)
-
-    cxxopts::Options options("dist_Icl input two binary", 
-        "Converts dist_Icl txt input file into binary format.\n\n"
-        "Input file has 5 columns: q, c, s, ss, se \n"
-        "Output file has 4 columns q*100+c, s, ss, se.\n");
-
-    std::string input, output;
-    options.add_options()
-        ("h,help", "Print usage")
-        ("i,input", "Input file path", cxxopts::value<std::string>())
-        ("o,output", "Output file path", cxxopts::value<std::string>())
-    ;
-
+    string input, output;
     
-    auto args = options.parse(argc, argv);
-
-
-    if (args.count("help"))
-    {
-      std::cout << options.help() << std::endl;
-      exit(0);
-    }
-    if (args.count("input")==1)
-        input = args["input"].as<std::string>();
-    else
-    {
-        std::cout << "Missing input flag: -i <filepath>" << std::endl;
-        exit(1);
-    }
-    if (args.count("output")==1)
-        output = args["output"].as<std::string>();
-    else
-    {
-        std::cout << "Missing input flag: -o <filepath>" << std::endl;
-        exit(1);
-    }
-    ////////////////////////////////////////////////////////////////////////
-
+    input=argv[1];
+    output=argv[2];
+    
 
     std::ifstream in(input);
     std::ofstream out(output, std::ios::binary);
